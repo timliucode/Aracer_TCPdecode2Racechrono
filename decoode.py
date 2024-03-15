1874575b36ed0ab31d977f843e8544a920a22965
TimLiu <c121122689@gmail.com>
2024-03-15 17:28:05 +0800

    upddate convert logic

--- decoode.py
+++ decoode.py
@@ -32,77 +32,45 @@ def convert(args):
         message = buffer[:length]  # 獲取一條數據
         buffer = buffer[length:]  # 從 buffer 中刪除這條數據
         if message[6:8] == bytes.fromhex(ID):  # 如果這條數據的ID是我們需要的
-            pass
+            if message[10] == 1:  # index:1
+                gps_utc_hh = message[11]
+                gps_utc_mm = message[12]
+                gps_utc_ss = message[13]
+                gps_utc_ms = int.from_bytes(message[14:16], byteorder='big')
+                gps_lat_deg = message[16]
+                gps_lat_min = message[17]
+            elif message[10] == 2:
+                gps_lat_sec = int.from_bytes(message[11:13], byteorder='big')
+                gps_lat_ns = chr(message[13])
+                gps_lon_deg = message[14]
+                gps_lon_min = message[15]
+                gps_lon_sec = int.from_bytes(message[16:18], byteorder='big')
+            elif message[10] == 3:
+                gps_lon_ew = chr(message[11])
+                gps_valid = message[12]
+                gps_speed = int.from_bytes(message[13:15], byteorder='big') * 0.539956803
+            elif message[10] == 4:
+                rpm = int.from_bytes(message[14:16], byteorder='big')
+                tps = message[16] / 255 * 100
+                vss1 = message[17]
+            elif message[10] == 5:
+                vss2 = message[11]
+                tc_lean_angle = message[12] - 127
+                tc_vss_fr_rate = message[13] * 0.78
+                volt_batt = message[14] / 10
+                t_eng = message[15] - 28
+                t_air = message[16] - 28
+                afr_wbo2_1 = message[17]
+            elif message[10] == 6:
+                afr_wbo2_2 = message[11]
+                cyl1_eng_ap = message[12]
+                cyl1_eng_ap_decimal = message[13]
+                racelanuh_en = message[14]
+                tc_status = message[15]
+
+                
 
 
-def get_values(dataReceived, id, index, valueindex):
-    for byte_str in dataReceived:
-        if byte_str[0:2] == bytes.fromhex(id) and byte_str[4:5] == bytes.fromhex(index):
-            return byte_str[valueindex[0]:valueindex[1]]
-    return b'\x00'
-
-
-def RC3(dataReceived):
-    global GPGGA
-    global GPRMC
-    global RC3d
-    GPS_UTC_hh = int.from_bytes(get_values(dataReceived, '0182', '01', (5, 6)), byteorder='big')
-    GPS_UTC_mm = int.from_bytes(get_values(dataReceived, '0182', '01', (6, 7)), byteorder='big')
-    GPS_UTC_ss = int.from_bytes(get_values(dataReceived, '0182', '01', (7, 8)), byteorder='big')
-    GPS_UTC_ms = int.from_bytes(get_values(dataReceived, '0182', '01', (9, 10)), byteorder='big')
-    GPS_Lat_deg = int(get_values(dataReceived, '0182', '01', (10, 11))[0] & 0x7F)
-    GPS_Lat_min = int.from_bytes(get_values(dataReceived, '0182', '01', (11, 12)), byteorder='big')
-    GPS_Lat_mmmm = int.from_bytes(get_values(dataReceived, '0182', '02', (5, 7)), byteorder='big')
-    GPS_Lat_NS = 'S' if (get_values(dataReceived, '0182', '02', (7, 8))[0] & 0x80) else 'N'
-    GPS_Lon_deg = int(get_values(dataReceived, '0182', '02', (8, 9))[0] & 0x7F)
-    GPS_Lon_min = int.from_bytes(get_values(dataReceived, '0182', '02', (9, 10)), byteorder='big')
-    GPS_Lon_mmmm = int.from_bytes(get_values(dataReceived, '0182', '02', (10, 12)), byteorder='big')
-    GPS_Lon_EW = 'W' if (get_values(dataReceived, '0182', '03', (5, 6))[0] & 0x80) else 'E'
-    GPS_Valid = 'A' if (get_values(dataReceived, '0182', '03', (6, 7))[0] & 0x80) else 'V'
-    GPS_Speed = int.from_bytes(get_values(dataReceived, '0182', '03', (7, 8)), byteorder='big')
-    TC_Xforce = ""
-    TC_Yforce = ""
-    TC_Zforce = ""
-    rpm = int.from_bytes(get_values(dataReceived, '0182', '04', (8, 10)), byteorder='big')
-    tps = int.from_bytes(get_values(dataReceived, '0182', '04', (10, 11)), byteorder='big')
-    vss = int.from_bytes(get_values(dataReceived, '0182', '04', (11, 12)), byteorder='big')
-    TC_Lean_Angle = ""
-    TC_VSSFRRate = int.from_bytes(get_values(dataReceived, '0182', '05', (7, 8)), byteorder='big')
-    Volt_batt = int.from_bytes(get_values(dataReceived, '0182', '05', (8, 9)), byteorder='big')
-    T_eng = int.from_bytes(get_values(dataReceived, '0182', '05', (9, 10)), byteorder='big')
-    T_air = int.from_bytes(get_values(dataReceived, '0182', '05', (10, 11)), byteorder='big')
-    AFR_Wbo2 = int.from_bytes(get_values(dataReceived, '0182', '06', (5, 6)), byteorder='big')
-    Cyl1_Eng_AP = int.from_bytes(get_values(dataReceived, '0182', '06', (6, 7)), byteorder='big')
-    MGU_A = ""
-    MGU_Temp = ""
-    MGU_eBoostTimer = ""
-    LaunchEN = ""
-    TCStatus = ""
-    SuspensionAD1 = ""
-    SuspensionAD2 = ""
-
-    if GPS_Valid == 'A':
-        fix = '1'
-    elif GPS_Valid == 'V':
-        fix = '0'
-
-    # $GPGGA,041245.800,2450.57532,N,12112.04516,E,2       ,8        ,1.08   ,311.00,M      ,    ,M      ,       ,       *7F
-    # $定位 ,時間       ,緯度      ,北,經度      ,東,定位品質,可見衛星數,水平精度,海拔  ,海拔單位,高程,高程單位,差分時間,差分站ID*校驗碼
-    # 定位品質說明:0=無效,1=GPS,2=DGPS,3=PPS,6=估算值
-    GPGGAo = f"GNGGA,{GPS_UTC_hh}{GPS_UTC_mm}{GPS_UTC_ss}.{GPS_UTC_ms},{GPS_Lat_deg}{GPS_Lat_min}.{GPS_Lat_mmmm},{GPS_Lat_NS},{GPS_Lon_deg}{GPS_Lon_min}.{GPS_Lon_mmmm},{GPS_Lon_EW},{fix},,,,,,,,"
-
-    # $GPRMC,041245.800,A   ,2450.57532,N,12112.04516,E,36.08       ,148.58,020122,     ,         ,       *1D
-    # $定位 ,時間       ,狀態,緯度      ,北,經度       ,東,速度(knot節),方位角 ,日月年,磁偏角,磁偏角方向,模式指示*校驗碼
-    # 狀態說明:A=有效定位，V=無效定位，一節=1.852公里/小時，模式指示說明:A=自動，D=差分，E=估算，N=數據無效，S=模擬
-    GPRMCo = f"GNRMC,{GPS_UTC_hh}{GPS_UTC_mm}{GPS_UTC_ss}.{GPS_UTC_ms},{GPS_Valid},{GPS_Lat_deg}{GPS_Lat_min}.{GPS_Lat_mmmm},{GPS_Lat_NS},{GPS_Lon_deg}{GPS_Lon_min}.{GPS_Lon_mmmm},{GPS_Lon_EW},{GPS_Speed},,{date},,,"
-
-    # $RC3,[time],[count],[xacc],[yacc],[zacc],[gyrox],[gyroy],[gyroz],[rpm/d1],[d2],[a1],[a2],[a3],[a4],[a5],[a6],[a7],[a8],[a9],[a10],[a11],[a12],[a13],[a14],[a15]*[checksum]
-    # 只要o2及qs能分開輸出AD就可使用於suspension
-    RC3do = f"RC3,{GPS_UTC_hh}{GPS_UTC_mm}{GPS_UTC_ss}.{GPS_UTC_ms},,{TC_Xforce},{TC_Yforce},{TC_Zforce},,,,{rpm},{tps},{vss},{TC_Lean_Angle},{TC_VSSFRRate},{Volt_batt},{T_eng},{T_air},{AFR_Wbo2},{Cyl1_Eng_AP},{MGU_A},{MGU_Temp},{MGU_eBoostTimer},{LaunchEN},{TCStatus},{SuspensionAD1},{SuspensionAD2}"
-
-    GPGGA = f"${GPGGAo}*{checksum(GPGGAo)}\n"
-    GPRMC = f"${GPRMCo}*{checksum(GPRMCo)}\n"
-    RC3d = f"${RC3do}*{checksum(RC3do)}\n"
 
 
 def checksum(cs):  # 計算NMEA0183校驗和
