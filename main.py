2d327d00729ad58b28d7ed32ee02a1d1dc2596be
TimLiu <c121122689@gmail.com>
2024-03-15 23:44:24 +0800

    Refactor main.py to improve ECU communication logic

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from twisted.internet import reactor, protocol, defer
 import decoode
 
 
+# 讀取 config.txt 檔案
 def read_config(config):
-    # 讀取 config.txt 檔案
     with open(config, 'r') as f:
         lines = f.readlines()
 
@@ -18,95 +18,99 @@ def read_config(config):
     return ip, watchdog, init
 
 
-# 這個class是用來與ECU通訊的
+# 這個class是用來處理ECU通訊的
 class EcuClient(protocol.Protocol):
+    # 初始化config.txt的內容
     def __init__(self):
         self.ip, self.watchdog, self.init = read_config('config.txt')
 
+    # 連線成功後執行
     def connectionMade(self):
         print("Connected to the server.")
         self.transport.setTcpNoDelay(True)  # 關閉 Nagle 算法
-        self.send_init()
-        reactor.callLater(1, self.send_watchdog)
+        self.send_init()  # 發送初始化數據
+        reactor.callLater(1, self.send_watchdog)  # 每1秒發送一次 watchdog
 
+    # 發送初始化數據
     def send_init(self):
         if self.init:
             item = self.init.pop(0)
             self.transport.write(item)
             reactor.callLater(0.01, self.send_init)
 
+    # 發送 watchdog
     def send_watchdog(self):
         self.transport.write(self.watchdog)
         reactor.callLater(1, self.send_watchdog)
 
+    # 接收數據
     def dataReceived(self, data):
         # 使用 Deferred 將接收到的數據轉交給外部函數處理
-        rc3 = defer.Deferred()
-        rc3.addCallback(decoode.convert)
-        rc3.addCallback(broadcast)
-        rc3.callback(data)
-
-def test(data):
-    print("data")
-    return data
+        rc3 = defer.Deferred()  # 創建一個Deferred對象
+        rc3.addCallback(decoode.convert)  # 添加一個外部decode回調函數
+        rc3.addCallback(broadcast)  # 添加一個向server發送數據的回調函數
+        rc3.callback(data)  # 調用callback方法，將數據傳遞給回調函數
 
 
+# 這個class是用來創建EcuClient的
 class EcuClientFactory(protocol.ReconnectingClientFactory):
     protocol = EcuClient
 
+    # 創建protocol
     def buildProtocol(self, addr):
         print("Building protocol...")
-        self.resetDelay()
+        self.resetDelay()  # 重置延遲
         return self.protocol()
 
-    def clientConnectionFailed(self, connector, reason):
-        print(f"Connection failed: {reason.getErrorMessage()}")
-        self.retry(connector)
 
-    def clientConnectionLost(self, connector, reason):
-        print(f"Connection lost: {reason.getErrorMessage()}")
-        self.retry(connector)
-
-
+# 這個class是用來處理RC3server通訊的
 class RC3server(protocol.Protocol):
+    # 連線成功後執行
     def connectionMade(self):
         print("Client connected")
-        if hasattr(self, 'factory'):
-            self.factory.clients.append(self)
+        if hasattr(self, 'factory'):  # 如果有factory屬性
+            self.factory.clients.append(self)  # 將自己添加到clients列表中
 
+    # 連線失敗後執行
     def connectionLost(self, reason):
         print("Connection lost")
-        if hasattr(self, 'factory'):
-            self.factory.clients.remove(self)
+        if hasattr(self, 'factory'):  # 如果有factory屬性
+            self.factory.clients.remove(self)  # 將自己從clients列表中移除
 
 
+# 這個class是用來創建RC3server的
 class RC3serverFactory(protocol.Factory):
+    # 初始化
     def __init__(self):
-        self.clients = []
+        self.clients = []  # 創建一個空的clients列表
 
+    # 創建protocol
     def buildProtocol(self, addr):
-        protocol_instance = RC3server()
+        protocol_instance = RC3server()  # 創建一個RC3server實例
         protocol_instance.factory = self  # 设置factory属性
-        return protocol_instance
+        return protocol_instance  # 返回protocol實例
 
 
+# 向所有客戶端廣播數據
 def broadcast(message):
-    clients = factory.clients
-    for client in clients:
-        client.transport.write(message.encode())
+    clients = factory.clients  # 獲取clients列表
+    for client in clients:  # 遍歷clients列表
+        client.transport.write(message.encode())  # 向每個客戶端發送數據
 
 
 if __name__ == '__main__':
-    ip, watchdog, init = read_config('config.txt')
-    print('ip:', ip)
+    ip, watchdog, init = read_config('config.txt')  # 讀取config.txt文件
 
-    server_ip = "192.168.1.220"
-    server_port = 12345
+    ecu_ip = "localhost"  # ECU的IP地址，測試時可以直接從這裡修改
+    # ecu_ip = ip         # ECU的IP地址，實際使用時從config.txt文件中讀取
+    ecu_port = 6666
 
-    reactor.connectTCP(server_ip, server_port, EcuClientFactory())
+    print('ip:', ecu_ip)
+    reactor.connectTCP(ecu_ip, ecu_port, EcuClientFactory()) # 連接ECU
 
-    port = 6666
-    factory = RC3serverFactory()
-    reactor.listenTCP(port, factory)
-    print(f"Server listening on port {port}")
+    factory = RC3serverFactory()    # 創建RC3serverFactory實例
+    ports = range(7777, 7781)       # 服務器端口範圍，這麼做的原因是方便racechrono選擇僅RC3還是RC3+NMEA
+    for port in ports:
+        reactor.listenTCP(port, factory)
+    print(f"Server listening on port {ports}")
     reactor.run()
