626fda91c323f672c47f89d096d0b6c3e5f92753
TimLiu <c121122689@gmail.com>
2024-03-23 02:45:59 +0800

    Refactor decoode.py to handle GPS invalid state and calculate bearing

--- decoode.py
+++ decoode.py
@@ -23,6 +23,8 @@ checksum是LRC校驗碼
 
 import ast
 import inspect
+import datetime
+import math
 
 ID = '0182'  # CAN ID (這是monitor的ID)
 length = 19  # 1個CAN包加上前綴及checksum的長度
@@ -37,22 +39,22 @@ def convert(data):
         if message[6:8] == bytes.fromhex(ID):  # 如果这条数据的ID是我们需要的
             match message[10]:
                 case 1:  # index:1
-                    gps_utc_hh = message[11]
-                    gps_utc_mm = message[12]
-                    gps_utc_ss = message[13]
-                    gps_utc_ms = int.from_bytes(message[14:16], byteorder='big')
-                    gps_lat_deg = message[16]
-                    gps_lat_min = message[17]
+                    gps_utc_hh = f"{message[11]:02d}"
+                    gps_utc_mm = f"{message[12]:02d}"
+                    gps_utc_ss = f"{message[13]:02d}"
+                    gps_utc_ms = f"{int.from_bytes(message[14:16], byteorder='big'):03d}"
+                    gps_lat_deg = f"{message[16]:02d}"
+                    gps_lat_min = f"{message[17]:02d}"
                 case 2:
-                    gps_lat_sec = int.from_bytes(message[11:13], byteorder='big')
+                    gps_lat_sec = f"{int.from_bytes(message[11:13], byteorder='big'):04d}"
                     gps_lat_ns = chr(message[13])
-                    gps_lon_deg = message[14]
-                    gps_lon_min = message[15]
-                    gps_lon_sec = int.from_bytes(message[16:18], byteorder='big')
+                    gps_lon_deg = f"{message[14]:02d}"
+                    gps_lon_min = f"{message[15]:02d}"
+                    gps_lon_sec = f"{int.from_bytes(message[16:18], byteorder='big'):04d}"
                 case 3:
                     gps_lon_ew = chr(message[11])
                     gps_valid = chr(message[12])
-                    gps_speed = int.from_bytes(message[13:15], byteorder='big') * 0.539956803
+                    gps_speed = f"{int.from_bytes(message[13:15], byteorder='big') * 0.539956803:.3f}"
                 case 4:
                     rpm = int.from_bytes(message[14:16], byteorder='big')
                     tps = message[16] / 255 * 100
@@ -77,15 +79,20 @@ def convert(data):
                     else:  # 如果GPS無效
                         quality = 0
 
+                    date = datetime.datetime.now(datetime.UTC).strftime('%d%m%y')
+
+                    bearingcalc = BearingCalculator()
+                    bearing = bearingcalc.calculate_bearing(gps_lat_sec, gps_lon_sec)
+
                     # $GNGGA,041245.800,2450.57532,N,12112.04516,E,2       ,8        ,1.08   ,311.00,M      ,    ,M      ,       , *7F
                     # $定位 ,時間       ,緯度      ,北,經度      ,東,定位品質,可見衛星數,水平精度,海拔  ,海拔單位,高程,高程單位,差分時間,差分站ID*校驗碼
                     # 定位品質說明:0=無效,1=GPS,2=DGPS,3=PPS,6=估算值
-                    GGA = f"GNGGA,{gps_utc_hh}{gps_utc_mm}{gps_utc_ss}.{gps_utc_ms},{gps_lat_deg}{gps_lat_min}.{gps_lat_sec}{gps_lat_ns},{gps_lon_deg}{gps_lon_min}.{gps_lon_sec}{gps_lon_ew},{quality},{gps_speed},,,,,,,,"
+                    GGA = f"GNGGA,{gps_utc_hh}{gps_utc_mm}{gps_utc_ss}.{gps_utc_ms},{gps_lat_deg}{gps_lat_min}.{gps_lat_sec},{gps_lat_ns},{gps_lon_deg}{gps_lon_min}.{gps_lon_sec},{gps_lon_ew},{quality},,,,M,,M,,"
 
-                    # $GPRMC,041245.800,A   ,2450.57532,N,12112.04516,E,36.08       ,148.58,020122,     ,         ,       *1D
+                    # $GNRMC,041245.800,A   ,2450.57532,N,12112.04516,E,36.08       ,148.58,020122,     ,         ,       *1D
                     # $定位 ,時間       ,狀態,緯度      ,北,經度       ,東,速度(knot節),方位角 ,日月年,磁偏角,磁偏角方向,模式指示*校驗碼
                     # 狀態說明:A=有效定位，V=無效定位，一節=1.852公里/小時，模式指示說明:A=自動，D=差分，E=估算，N=數據無效，S=模擬
-                    RMC = f"GPRMC,{gps_utc_hh}{gps_utc_mm}{gps_utc_ss}.{gps_utc_ms},{gps_valid},{gps_lat_deg}{gps_lat_min}.{gps_lat_sec}{gps_lat_ns},{gps_lon_deg}{gps_lon_min}.{gps_lon_sec}{gps_lon_ew},{gps_speed},,,,,"
+                    RMC = f"GNRMC,{gps_utc_hh}{gps_utc_mm}{gps_utc_ss}.{gps_utc_ms},{gps_valid},{gps_lat_deg}{gps_lat_min}.{gps_lat_sec},{gps_lat_ns},{gps_lon_deg}{gps_lon_min}.{gps_lon_sec},{gps_lon_ew},{gps_speed},{bearing},{date},,,A"
 
                     # $RC3,[time],[count],[xacc],[yacc],[zacc],[gyrox],[gyroy],[gyroz],[rpm/d1],[d2],[a1],[a2],[a3],[a4],[a5],[a6],[a7],[a8],[a9],[a10],[a11],[a12],[a13],[a14],[a15]*[checksum]
                     RC3 = f"RC3,{gps_utc_hh}{gps_utc_mm}{gps_utc_ss}.{gps_utc_ms},,,,,,,,{rpm},{tps},{vss1},{vss2},{tc_lean_angle},{tc_vss_fr_rate},{volt_batt},{t_eng},{t_air},{afr_wbo2_1},{afr_wbo2_2},{cyl1_eng_ap},{cyl1_eng_ap_decimal},{racelanuh_en}"
@@ -97,6 +104,62 @@ def convert(data):
                     result = GNGGA + GNRMC + RC3out
                     return result
     return ""
+
+
+class BearingCalculator:
+    def __init__(self):
+        self.lat1 = "1"
+        self.lon1 = "1"
+        self.output = "0"
+
+    def calculate_bearing(self, lat, lon):
+        """
+        計算從(self.lat1, self.lon1)點指向(lat2, lon2)點的航向角
+        僅使用經緯度的小數部分
+
+        參數:
+        lat (str): 緯度的小數部分
+        lon (str): 經度的小數部分
+
+        返回:
+        bearing (float): 航向角(0-360度)
+        """
+        # 轉為小數
+        lat1_dec = "0." + str(self.lat1)
+        lon1_dec = "0." + str(self.lon1)
+        lat2_dec = "0." + str(lat)
+        lon2_dec = "0." + str(lon)
+
+        # 上一次的經緯度
+        self.lat1 = lat
+        self.lon1 = lon
+
+        # 轉換為float
+        lat1_min = float(lat1_dec)
+        lon1_min = float(lon1_dec)
+        lat2_min = float(lat2_dec)
+        lon2_min = float(lon2_dec)
+
+        # 計算差值
+        dlat = lat2_min - lat1_min
+        dlon = lon2_min - lon1_min
+
+        # 計算方位角
+        y = math.sin(dlon) * math.cos(lat2_min)
+        x = math.cos(lat1_min) * math.sin(lat2_min) - math.sin(lat1_min) * math.cos(lat2_min) * math.cos(dlon)
+        bearing = math.atan2(y, x)
+        bearing_degrees = math.degrees(bearing)
+
+        # 範圍0-360度
+        bearing_degrees = (bearing_degrees + 360) % 360
+
+        self.output = f"{bearing_degrees:.2f}"
+
+        if bearing_degrees != 0:
+            output = self.output
+            return output
+        else:
+            return self.output
 
 
 def checksum(cs):  # 計算NMEA0183校驗和
